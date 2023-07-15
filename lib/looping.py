68baa9061dceb6b36ab4d20cc9a7605eeb188166
George8918 <GZheng8918@gmail.com>
2023-07-15 12:56:44 -0700

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -20,4 +20,14 @@ def square_integers(int_list):
 
 def fizzbuzz():
     # code goes here!
+    for i in range (1,101) :
+        if i % 3 ==0 and i % 5 == 0:
+            print ("FizzBuzz")
+        elif i % 3 ==0:
+            print ("Fizz")
+        elif i % 5 ==0:
+            print ("Buzz")
+        else:
+            print (i)
+
     pass
